f90db8a845d1b35bd04db2555b47bc013d6fee9e
pardhuva <pardhuvabheemarati@gmail.com>
2025-11-30 15:06:41 +0530

    Updated index.js

--- api/index.js
+++ api/index.js
@@ -19,8 +19,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 
-// ------------------------------------
-
 const app = express();
 
 // Serve images
